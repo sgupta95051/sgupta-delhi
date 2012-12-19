c4226cbeeeb99464122b946c3d7cd6b17c55ae4d
Sunil Gupta <sunil_k_gupta@hotmail.com>
2012-12-19 15:58:55 +0530

    Added new file.

---
+++ TestGitInstallation.java
@@ -0,0 +1,12 @@
+package sorting;
+public class TestGitInstallation{
+
+	public sttaic void main(String[] args){
+	// Test files
+		System.out.println("Git is funsss!!!");
+		System.out.println("Git is cool!!!");
+				
+		System.out.println("Git is funsss Hellos!!!");
+	}
+
+}
